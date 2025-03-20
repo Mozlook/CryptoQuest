449f0439709b81c6ef260ea67ac12525c535f037
Mikolaj Mozoluk <mikolaj.moozoluk@gmail.com>
2025-03-20 14:37:34 +0100

    some UI shit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,9 +2,9 @@ export default function Header(){
     return(
         <header>
             <div className="button-container">
-                <button>About</button>
-                <button>PlaceHolder</button>
-                <button>Issues</button>
+                <button><i class="fas fa-info-circle"></i> About</button>
+                <button><i class="fas fa-bug"></i>Report Issue</button>
+                <button><i class="fas fa-cog"></i>PlaceHolder</button>
             </div>
         </header>
     )
--- src/components/SubmitForm.jsx
+++ src/components/SubmitForm.jsx
@@ -1,8 +1,11 @@
 export default  function SubmitForm(){
     return(
-    <form>
-        <input type="text" id="kod" name="kod" placeholder="Type code here" />
-        <button>Submit</button>
-    </form>
+        <div className="form-container">
+            <h3>Submit Your Answer:</h3>
+            <form>
+                <input type="text" id="kod" name="kod" placeholder="Type code here..." />
+                <button>Submit</button>
+            </form>
+        </div>
     )
 }
