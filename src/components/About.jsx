b7da0419015a58a3ce7df784d62ff44e66b739b4
Mikolaj Mozoluk <mikolaj.moozoluk@gmail.com>
2025-03-22 15:56:01 +0100

    About and Report Issue styling

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,4 +1,4 @@
-import "./About.css"
+import "../Styles/About.css"
 
 export default function About({setIsAboutOpen}){
     return (
@@ -8,7 +8,7 @@ export default function About({setIsAboutOpen}){
             <div>
                 
                 <p>CryptoQuest is an exciting code-breaking and deciphering game where your goal is to crack codes based on the clues provided on the screen to advance to the next level. You can tackle the puzzles solo or team up with friends for a collaborative experience. The first three puzzles are designed to ease you into the game and show you the ropes, but after that, the difficulty ramps up – are you ready for the challenge?</p>
-                <button onClick={() => setIsAboutOpen(false)}>
+                <button className="close"onClick={() => setIsAboutOpen(false)}>
                 Close
                 </button>
             </div>
--- src/components/IssueForm.jsx
+++ src/components/IssueForm.jsx
@@ -1,4 +1,24 @@
-export default function IssueForm(){
-    <div className="overlay" onClick={() => setIsAboutOpen(false)}>
+import "../Styles/IssueForm.css"
+export default function IssueForm({setIsIssueFormOpen}){
+    return(
+        <div className="overlay" onClick={() => setIsIssueFormOpen(false)}>
+        <div className="issue-form-container" onClick={(e) => e.stopPropagation()}>
+            <p>Report an Issue</p>
+            <form>
+                <label>Name or number of puzzle</label>
+                <input type="text" id="puzzle_name" name="puzzle_name" placeholder="Cezar cipher" />
+                <label>Describe the issue</label>
+                <textarea rows="10" cols="50" id="issue_description" name="issue_description" placeholder="Describe your issue here..."/>
+                <div className="buttons-container">
+                    <button className="submit-issue">
+                        Submit
+                    </button>
+                    <button className="cancel" onClick={() => setIsIssueFormOpen(false)}>
+                        Cancel
+                    </button>
+                </div>
+            </form>
+        </div>
     </div>
+    )
 }
