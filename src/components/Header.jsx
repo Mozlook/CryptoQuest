7f0b564b963f0ed672329df08c429212aeb8803a
Mikolaj Mozoluk <mikolaj.moozoluk@gmail.com>
2025-04-08 16:36:01 +0200

    hid placeholder button

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,19 @@
-export default function Header({ setIsAboutOpen, setIsIssueFormOpen, setPuzzleId}){
-    return(
-        <header>
-            <div className="button-container">
-                <button onClick={() => setIsAboutOpen(true)} ><i className="fas fa-info-circle"></i> About</button>
-                <button onClick={() => setIsIssueFormOpen(true)}><i className="fas fa-bug"></i>Report Issue</button>
-                <button onClick={() => setPuzzleId(prev => prev + 1)}><i className="fas fa-cog"></i>PlaceHolder</button>
-            </div>
-        </header>
-    )
+export default function Header({
+	setIsAboutOpen,
+	setIsIssueFormOpen,
+	setPuzzleId,
+}) {
+	return (
+		<header>
+			<div className="button-container">
+				<button onClick={() => setIsAboutOpen(true)}>
+					<i className="fas fa-info-circle"></i> About
+				</button>
+				<button onClick={() => setIsIssueFormOpen(true)}>
+					<i className="fas fa-bug"></i>Report Issue
+				</button>
+				{/* <button onClick={() => setPuzzleId(prev => prev + 1)}><i className="fas fa-cog"></i>PlaceHolder</button> */}
+			</div>
+		</header>
+	);
 }
